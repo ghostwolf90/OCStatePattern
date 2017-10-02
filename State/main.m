964c3ff170b2c385093941244d056fc1f284ca6b
laibit <ghostwolf90@gmail.com>
2017-10-03 07:43:48 +0800

    Initial Commit

---
+++ State/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  State
+//
+//  Created by Laibit on 2017/10/3.
+//  Copyright © 2017年 Laibit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
